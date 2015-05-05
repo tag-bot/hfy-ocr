bd64bdfe0c35cbfe38ab14bdb6a15fdc02613ae8
zeteref <zeteref@live.com>
2015-05-05 15:33:33 +0200

    initial commit

---
+++ nocr_image.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import sys
+from os import path
+
+KEY='1f90833f0cd9d3403c32fff56dc7e4ba'
+
+def upload_image(fpath):
+    fname = path.basename(fpath)
+    result = requests.post('http://api.newocr.com/v1/upload?key=%s' % KEY, files=[('file', (fname, open(fpath, 'rb'), 'image/jpg' ))])
+
+    if result.status_code == 200:
+        content = json.loads(result.content)
+        file_id = content['data']['file_id']
+        pages = content['data']['pages']
+
+        return (file_id, pages)
+
+    print 'Unable to upload image', result.status_code, result.content
+    sys.exit()
+
+
+def ocr_image(fpath):
+    file_id, pages = upload_image(fpath)
+
+    for page in range(1, int(pages)+1):
+        result = requests.get('http://api.newocr.com/v1/ocr', params={'file_id':file_id, 'lang':'eng', 'page':page, 'key':KEY})
+
+        if result.status_code == 200:
+            content = json.loads(result.content)
+            text = content['data']['text']
+            
+            print text.encode('utf-8')
+
+    print 'Unable to OCR image', result.status_code, result.content
+
+
+def format_for_reddit(text):
+    lines = text.split('\n')
+    ret = []
+
+    for line in lines:
+        if line.startswith('>>') or line.startswith('Anonymous'):
+            continue
+        if line == '':
+            ret += ['']
+
+        ret += [line]
+
+    return '\n'.join(ret)
+
+#ocr_image('/tmp/test.jpg')
+
+f = open('/tmp/a.txt', 'r')
+txt = f.read()
+f.close()
+
+print format_for_reddit(txt)
